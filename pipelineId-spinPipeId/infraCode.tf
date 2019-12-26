5353934201782f096e2274e6d52eb2bd19461556
OpsMx <Team@OpsMx.com>
2019-12-26 20:16:01 +0000

    adding spinterra plan state

--- pipelineId-spinPipeId/infraCode.tf
+++
@@ -1,1 +0,0 @@
-module "terraModule"{source = "git::https://lalitv92:Vikram92!@github.com/lalitv92/TerraformPlansModule.git//namespace"}
--- pipelineId-spinPipeId/provider.tf
+++
@@ -1,1 +0,0 @@
-provider "kubernetes"{ config_path = "/home/terraspin/opsmx/kubeaccount/my-k8s-v2-account" }
